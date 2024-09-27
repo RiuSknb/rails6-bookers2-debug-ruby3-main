8fc06494a268760bfb1e84f070b895448d5a02f2
RiuSknb <177892055+RiuSknb@users.noreply.github.com>
2024-09-27 04:44:50 +0000

    [Correct]users_index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top, :about] # top, about の2つのアクションのみ、ログイン無しでもアクセス可能にする
+  # before_action :authenticate_user!, except: [:top, :about] # top, about の2つのアクションのみ、ログイン無しでもアクセス可能にする
   before_action :configure_permitted_parameters, if: :devise_controller?
+#   before_action :authenticate_user!は、deviseを入れると使えるようになるヘルパーメソッドであり、
+# コントローラーに設定することで、「ログイン認証されていなければ、ログイン画面へリダイレクトする」機能を実装できます。
+# controller/application_controller.rbに書いてしまうと、
+# 全コントローラに適用されてしまい、ログインが関係ないところにも実行されてしまうため、
+# 制限が必要ない、トップページ・アバウトページにもかかってしまいます。
+# bookとuserのcontrollerだけに、before_action :authenticate_user!を追記してあげましょう。
+
+
 
   private
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
