9d71471491604619f6ca738b54db9e3ac99468a2
RiuSknb <177892055+RiuSknb@users.noreply.github.com>
2024-09-27 04:35:36 +0000

    [Correct]routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get "home/about"=>"homes#about"
-
+# deviseのルーティングより上にresources :users, ~~~の記述が存在するため、ログインページ(users/sign_in)に
+# アクセスをしてもusersのshowページとして呼び出されます。
+# usersコントローラーにはログイン制限を掛けているため、ログインページへのリダイレクトが実行され再びログインページが呼び出されますが、これもusersのshowページとして呼び出されます。
+# このようにresources :usersの記述が上にあることによりログインページにアクセスしても、usersのshowページとして呼び出されるため無限ループが発生している状態です。
+# これを解消するために、devise_forをresources :usersの記述より上にすることでusers/sign_inといったURLはdeviseのコントローラーで処理されるようになります。
   resources :books, only: [:new, :index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
 
