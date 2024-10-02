a8460a12c0614e2fd5d29c052ce9293fe3940033
ChiakiTanaka <177892055+RiuSknb@users.noreply.github.com>
2024-10-02 07:01:50 +0000

    [Update]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,10 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # config.hosts << "a8c9cb2b77b24968bf1f0967084f4a3f.vfs.cloud9.us-east-1.amazonaws.com"
-  config.hosts << "0e9ab2b9bf9043b4919b53ef43b6e56a.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  # config.hosts << "0e9ab2b9bf9043b4919b53ef43b6e56a.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  # ↑をコメントアウト。
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "musical-happiness-4jg5j777wwv63jwx-3000.app.github.dev"
+  # ↑コメントイン
 end
