1ae589a84174de987ae3629aadd1e4b7fcc50920
EC2 Default User <ec2-user@ip-172-31-7-137.ap-northeast-1.compute.internal>
2024-10-03 08:52:51 +0000

    [Update]search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,34 @@
 class SearchesController < ApplicationController
-  
+
   def search
     @model = params[:model]  # ユーザーか投稿かを選択
     @keyword = params[:keyword]  # 検索キーワード
-    
+    @method = params[:search_method] # 検索手法
+
     if @model == "user"
       @results = User.where("name LIKE ?", "%#{@keyword}%")
     elsif @model == "book"
       @results = Book.where("title LIKE ?", "%#{@keyword}%")
+    end
+  end
+
+
+  private
+
+
+    # 検索メソッドのロジック
+  def search_for(model, attribute)
+    case @method
+    when 'match'
+      model.where("#{attribute} LIKE ?", "#{@keyword}")
+    when 'forward'
+      model.where("#{attribute} LIKE ?", "#{@keyword}%")
+    when 'backward'
+      model.where("#{attribute} LIKE ?", "%#{@keyword}")
+    when 'partial'
+      model.where("#{attribute} LIKE ?", "%#{@keyword}%")
+    else
+      model.none  # 手法が選択されていない場合は空の結果
     end
   end
 end
